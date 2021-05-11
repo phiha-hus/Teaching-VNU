124f84a93e1b6a8a9a68916307dc11783456e6c7
phiha-hus <haphi@hus.edu.vn>
2021-05-11 08:36:13 +0700

    add new materials

--- Huong_dan_SV_va_ThS/lambertw_dde/find_S_VA.m
+++ Huong_dan_SV_va_ThS/lambertw_dde/find_S_VA.m
@@ -1,7 +1,8 @@
 function [K2] = find_S_VA(h, A, Ad)
 global	h	A	Ad
 
-h=1;	A = 1;	Ad = 1;	N = 50; t0 = 0
+tic 
+h = 1;	A = 1;	Ad = 1;	N = 20; t0 = 0 ; 
 a = A; ad = Ad; 
 
 W0 = lambertw_matrix(0,-h*Ad*expm(h*A));
@@ -10,32 +11,30 @@ alpha = real(W0/h - A)
 S = [];
 % T chay tren [h,\infty)
 h1 = 1e-2;
-T = h : h1 : 5*h; 
+T = h : h1 : 20*h; 
+[~,nT] = size(T);
 
-J2N = zeros(size(T));
+J2N = zeros(2*N+1);
 
-for i = 1:length(T)
-  J2Nt = 0; 
-  for	k = -N : N  
+for	k = -N : N  
     k; 
+    
     W = fsolve(@witer,W0);
     Sk = (1/h) * W	+	A;
     lambda = eig(Sk);
     S = [S Sk];
-    %pause
-  
-    mauso_k = 1 - ad * h * expm(-Sk * h) ;
-    tuso_k = exp((Sk - alpha) * t)  ;
+    %pause    
+    
+    tuso_k = exp((Sk - alpha) * T)  ;
+    mauso_k = 1 - ad * h * expm(-Sk * h) ;  
     phanso_k = tuso_k./mauso_k;
   
     tong_phan_so = 0;
     tong_phan_so = tong_phan_so + phanso_k  
-   end
-   J2Nt = norm(tong_phan_so) ; 
    
    J2N(i) = J2Nt ;
 end
 
 K2 = max(J2N) ; 
 
-
+toc
